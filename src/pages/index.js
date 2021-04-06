e70afffb7f323b0abd6fd84dbd06a04f4cb920cd
Gabriel Dias Mendonça <71290629+gaabrielo@users.noreply.github.com>
2021-04-06 13:42:38 -0300

    Layout Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-
 import { Button } from '@material-ui/core';
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <div>
+    <Layout title="YouTube">
       Youtube clone
       <Button variant="outlined" color="secondary">
         Hello World
       </Button>
-    </div>
+    </Layout>
   );
 }
